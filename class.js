4bb9b05b17ab46587e2e2574ccbed87d0081a15e
Your Name <akecakabro@gmail.com>
2021-09-22 14:01:26 +0200

    add class.js

---
+++ class.js
@@ -0,0 +1,35 @@
+
+        students = [
+    {name: "Jake", birthYear: 1980, marks: 80},
+    {name: "John", birthYear: 1979, marks: 60},
+    {name: "Abby", birthYear: 1990, marks: 70},
+    {name: "Carter", birthYear: 2000, marks: 85},
+    {name: "Dave", birthYear: 2010, marks: 62},
+    {name: "Allison", birthYear: 1967, marks: 49},
+    {name: "Isak", birthYear: 1949, marks: 30},
+    {name: "Gerald", birthYear: 1996, marks: 83},
+    {name: "George", birthYear: 2001, marks: 19},
+] 
+
+   class student {
+       constructor(birthYear,marks,name){
+           this.birthYear=birthYear
+           this.marks=marks
+           this.name=name
+       }
+       getage(){
+           return 2021-this.birthYear
+       }
+       getYear(){
+           return this.marks
+       }
+       getName(){
+          return this.name
+       }
+   }
+   const stud1= new student(2001,19,"alex")
+   console.log(stud1.getage())
+   console.log(stud1.getYear())
+   console.log(stud1.getName())
+
+    
